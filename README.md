9cd30ab4b6dbaa309c57c1f54af2442987dcfffa
LingYuZhao <liming7887@qq.com>
2024-03-18 18:38:30 +0800

    update

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ a compact plugin package that can achieve pop-up display on front-end pages.
 <script>
     window.onload = () => {
         // 创建弹窗 传递一个元素进去
-        const pop_up = new JokerBox_popUp(
+        const pop_up = JokerBox_popUp.create(
                 document.getElementById("b"),
                 // 在构造函数的第二个参数可以传递一些样式 TODO 当然 您也可以直接使用 CSS 选择器直接修改样式
                 {
@@ -66,6 +66,57 @@ a compact plugin package that can achieve pop-up display on front-end pages.
         // 显示弹窗 在这里代表显示 10000 毫秒
         pop_up.show("这里是弹窗内容", 10000)
     }
+</script>
+</html>
+```
+
+### 设置格式的使用示例
+
+您可以直接通过 `JokerBox_popUp.createByHtml` 创建出来一个以您写好的HTML元素做为格式的弹窗对象，这有助于您直接使用您的预制格式，增加灵活性，本示例将为您展示如何使用您自己的自定义
+HTML 格式来进行展示！
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pop_up</title>
+</head>
+<body id="b">
+<p>这里是原本的网页内容</p>
+
+<!-- 创建一个自己的自定义样式的元素 在这里我们准备了一个HTML元素 TODO 需要做为我们的预制格式 -->
+<div class="my_pop">
+    <!-- 如果设置类名为 pop_up_content_text 代表这个元素将做为一个文本展示元素 -->
+    <h1 class="pop_up_content_text" style="border: blue solid 1px"></h1>
+    <hr>
+    <!-- 如果设置类名为 pop_up_content_close 代表这个元素将做为一个关闭按钮元素 -->
+    <h3 class="pop_up_content_close" style="border: blueviolet solid 1px"></h3>
+</div>
+
+</body>
+
+<!-- 在下面直接进行引用即可！ -->
+<link href="css/joker_box_pop_up.css" rel="stylesheet">
+<script src="./js/joker_box_pop_up.js"></script>
+<script>
+
+    // 创建弹窗 使用静态函数 createByHtml 可以传递一个自定义格式 HTML 元素进去
+    const pop_up = JokerBox_popUp.createByHtml(
+            // 第一个元素还是用来承载弹窗的元素
+            document.getElementById("b"),
+            // TODO 第二个元素就是我们准备的自定义格式的 HTML 元素的 CSS 路径！
+            ".my_pop",
+            // 在构造函数的第二个参数可以传递一些样式 TODO 当然 您也可以直接使用 CSS 选择器直接修改样式
+            {
+                // 背景样式
+                background: "url('https://www.lingyuzhao.top/image/graphic-loading1.jpg')",
+                // 字体颜色样式
+                color: '#fff'
+            }
+    );
+    // 显示弹窗 在这里代表显示 10000 毫秒
+    pop_up.show("这里是弹窗内容", 10000)
 </script>
 </html>
 ```
--- js/joker_box_pop_up.js
+++ js/joker_box_pop_up.js
@@ -4,11 +4,29 @@
 class JokerBox_popUp {
 
     /**
+     * 将弹窗功能装载到一个元素上，这将会把弹窗组件的声明周期与元素的生命周期绑定在一起
      *
+     * Load the pop-up function onto an element, which will bind the declaration cycle of the pop-up component to the lifecycle of the element
      * @param dom {HTMLElement} 需要承载弹窗的元素
+     *
+     * Elements that need to carry pop-ups
+     *
+     * @param cssFormatter {string} 在弹窗中显示内容时要使用的预制格式，我们允许并支持您使用自己的格式做为弹窗中的展示，请注意，这将会让您的元素被 jokerBox 管理！
+     * We allow and support you to use your own pre made format for displaying content in pop ups. Please note that this will allow your elements to be managed by jokerBox!
+     *
+     * Elements that need to carry pop-ups
      * @param color {{color: string, background: string}} 分别是背景颜色和字体颜色
+     *
+     *
+     * They are background color and font color, respectively
+     * @param closeClickAnimation 关闭按钮被点击之后的关闭动画效果 在这里使用的是内置的动画，如果您有自己的动画，可以直接在这里传递动画的名字！
+     *
+     * The close animation effect after the close button is clicked here uses the built-in animation. If you have your own animation, you can directly pass the name of the animation here!
      */
-    constructor(dom, color = {}) {
+    constructor(dom,
+                color = {},
+                closeClickAnimation = 'jokerBox_pop_up_show_display_life_close',
+                cssFormatter = undefined) {
 
         console.info(`jokerBox_v${JokerBox_popUp.getVersion()} -> popUp set ok!!`)
 
@@ -22,30 +40,58 @@ class JokerBox_popUp {
             color['color'] = JokerBoxUtils.invertColor(color['background']);
         }
 
-        let div1 = document.createElement("div");
-        div1.id = "jokerBox_pop_up_show_div";
+        let div1, div2, div3;
 
-        let div2 = document.createElement("div");
-        div2.className = "pop_up_content_text";
+        if (cssFormatter !== undefined) {
+            div1 = document.querySelector(cssFormatter);
+            const s1 = cssFormatter + ' > .pop_up_content_text';
+            const s2 = cssFormatter + ' > .pop_up_content_close';
+            div2 = document.querySelector(s1);
+            div3 = document.querySelector(s2);
+            if (!(div1 && div2 && div3)) {
+                console.error("jokerBox_popUp: subHtml is undefined!!\n" +
+                    "You need to ensure that the following two class name elements are included in the format you specify\n" +
+                    "\n" +
+                    "1. " + s1 + ": Text content display element（There can only be one）\n" +
+                    "\n" +
+                    "2. " + s2 + ": Close button display element（There can only be one）", {
+                    "your pop_up-content_text": div2,
+                    "your pop_up-content_close": div3
+                });
+                return;
+            }
+        } else {
+            div1 = document.createElement("div");
+            div2 = document.createElement("div");
+            div3 = document.createElement("a");
+            div1.classList.add('jokerBox_pop_up_show_div')
+            div2.classList.add('pop_up_content_text');
+            div3.classList.add('pop_up_content_close');
+
+            div1.appendChild(div3);
+            div1.appendChild(div2);
+            dom.appendChild(div1);
+        }
+
         div2.innerText = 'welcome to use jokerBox!!!';
-
-        let div3 = document.createElement("a");
-        div3.className = "pop_up_content_close";
         div3.innerText = 'x'
         div3.title = '关闭弹窗'
         div3.href = '#'
         div3.onclick = () => {
-            div1.style.animation = 'jokerBox_pop_up_show_display_life_close 1s forwards';
+            div1.style.animation = closeClickAnimation + ' 1s forwards';
         }
-        div1.appendChild(div3);
-        div1.appendChild(div2);
-        dom.appendChild(div1);
+
 
         this.dom = div1;
         this.textDom = div2;
 
         this.dom.style.background = color['background'];
+        this.dom.style.transform = 'scale(0)';
+        this.dom.style.height = '0';
         for (let childNode of this.dom.childNodes) {
+            if (childNode.style === undefined) {
+                continue;
+            }
             childNode.style.color = color['color'];
         }
 
@@ -63,6 +109,51 @@ class JokerBox_popUp {
         this.dom.onanimationend = function () {
             this.style.animation = 'none';
         }
+    }
+
+    /**
+     * 将弹窗功能装载到一个元素上，这将会把弹窗组件的声明周期与元素的生命周期绑定在一起
+     *
+     * Load the pop-up function onto an element, which will bind the declaration cycle of the pop-up component to the lifecycle of the element
+     * @param dom {HTMLElement} 需要承载弹窗的元素
+     *
+     *
+     * @param cssFormatter {string} 在弹窗中显示内容时要使用的预制格式，我们允许并支持您使用自己的格式做为弹窗中的展示，请注意，这将会让您的元素被 jokerBox 管理！
+     * We allow and support you to use your own pre made format for displaying content in pop ups. Please note that this will allow your elements to be managed by jokerBox!
+     *
+     * Elements that need to carry pop-ups
+     * @param color {{color: string, background: string}} 分别是背景颜色和字体颜色
+     *
+     *
+     * They are background color and font color, respectively
+     * @param closeClickAnimation 关闭按钮被点击之后的关闭动画效果 在这里使用的是内置的动画，如果您有自己的动画，可以直接在这里传递动画的名字！
+     *
+     * The close animation effect after the close button is clicked here uses the built-in animation. If you have your own animation, you can directly pass the name of the animation here!
+     */
+    static createByHtml(dom, cssFormatter,
+                        color = {},
+                        closeClickAnimation = 'jokerBox_pop_up_show_display_life_close') {
+        return new JokerBox_popUp(dom, color, closeClickAnimation, cssFormatter);
+    }
+
+    /**
+     * 将弹窗功能装载到一个元素上，这将会把弹窗组件的声明周期与元素的生命周期绑定在一起
+     *
+     * Load the pop-up function onto an element, which will bind the declaration cycle of the pop-up component to the lifecycle of the element
+     * @param dom {HTMLElement} 需要承载弹窗的元素
+     *
+     * Elements that need to carry pop-ups
+     * @param color {{color: string, background: string}} 分别是背景颜色和字体颜色
+     *
+     * They are background color and font color, respectively
+     * @param closeClickAnimation 关闭按钮被点击之后的关闭动画效果 在这里使用的是内置的动画，如果您有自己的动画，可以直接在这里传递动画的名字！
+     *
+     * The close animation effect after the close button is clicked here uses the built-in animation. If you have your own animation, you can directly pass the name of the animation here!
+     */
+    static create(dom,
+                  color = {},
+                  closeClickAnimation = 'jokerBox_pop_up_show_display_life_close') {
+        return this.createByHtml(dom, undefined, color, closeClickAnimation)
     }
 
     static getVersion() {
